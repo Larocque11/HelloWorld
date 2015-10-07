727e8c8982921593361f57548b4ee451963c53b9
Andy931 <andy.liu@ocsbstudent.ca>
2015-10-07 14:19:34 -0400

    Added the button

--- HelloWorld/HelloWorld/frmHelloWorld.Designer.cs
+++ HelloWorld/HelloWorld/frmHelloWorld.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblHelloWorld
@@ -41,11 +42,22 @@
             this.lblHelloWorld.TabIndex = 0;
             this.lblHelloWorld.Text = "Hello, World!";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(133, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // frmHelloWorld
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 113);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblHelloWorld);
             this.Name = "frmHelloWorld";
             this.Text = "Hello, World!";
@@ -57,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Label lblHelloWorld;
+        private System.Windows.Forms.Button button1;
     }
 }
 
